4b8a6ed590442652001033f24645b769c2b153f1
ruyisbaros <ahmet.erdonmez77@gmail.com>
2024-11-19 23:44:54 +0100

    code refactored

--- 2-calcculator/main.py
+++ 2-calcculator/main.py
@@ -2,73 +2,74 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QLabel, QWidget, QPushButton,
                              QVBoxLayout, QHBoxLayout, QGridLayout, QLineEdit, QVBoxLayout)
 
-app = QApplication([])
-# Main window
-main_window = QWidget()
-main_window.setWindowTitle('Calculator App')
-main_window.resize(250, 300)
 
+class CalculatorApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Calculator App')
+        self.resize(250, 300)
 
-def button_clicked():
-    button = app.sender()
-    text = button.text()
+        # Objects/Widgets
+        self.text_box = QLineEdit()
+        self.grid = QGridLayout()
 
-    if text == '=':
-        try:
-            result = eval(text_box.text())
-            text_box.setText(str(result))
-            text_box.setFocus()
-        except ZeroDivisionError:
-            text_box.setText('Error: Division by zero')
-            text_box.setFocus()
-
-    else:
-        text_box.setText(text_box.text() + text)
-        text_box.setFocus()
-
-
-# Objects/Widgets
-text_box = QLineEdit()
-grid = QGridLayout()
-
-# Buttons
-buttons = [
-    '7', '8', '9', '/',
-    '4', '5', '6', '*',
-    '1', '2', '3', '-',
-    '0', '.', '=', '+',
-]
-
-clear = QPushButton('C')
-clear.clicked.connect(lambda: text_box.setText(''))
-delete = QPushButton('<-')
-delete.clicked.connect(lambda: text_box.backspace())
-
-row = 0
-col = 0
-for button in buttons:
-    button_obj = QPushButton(button)
-    button_obj.clicked.connect(button_clicked)
-    grid.addWidget(button_obj, row, col)
-    col += 1
-    if col == 4:
+        # Buttons
+        self.buttons = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            '0', '.', '=', '+',
+        ]
+        row = 0
         col = 0
-        row += 1
+        for button in self.buttons:
+            button_obj = QPushButton(button)
+            button_obj.clicked.connect(self.button_clicked)
+            self.grid.addWidget(button_obj, row, col)
+            col += 1
+            if col == 4:
+                col = 0
+                row += 1
 
-# Design the calculator layout
-main_layout = QVBoxLayout()
-main_layout.addWidget(text_box)
-main_layout.addLayout(grid)
+        self.clear = QPushButton('C')
+        self.clear.clicked.connect(lambda: self.text_box.setText(''))
+        self.delete = QPushButton('<-')
+        self.delete.clicked.connect(lambda: self.text_box.backspace())
 
-button_rows = QHBoxLayout()
-button_rows.addWidget(clear)
-button_rows.addWidget(delete)
+        # Design the calculator layout
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.text_box)
+        main_layout.addLayout(self.grid)
 
+        button_rows = QHBoxLayout()
+        button_rows.addWidget(self.clear)
+        button_rows.addWidget(self.delete)
 
-main_layout.addLayout(button_rows)
+        main_layout.addLayout(button_rows)
 
-main_window.setLayout(main_layout)
+        self.setLayout(main_layout)
+
+    def button_clicked(self):
+        button = app.sender()
+        text = button.text()
+
+        if text == '=':
+            try:
+                result = eval(self.text_box.text())
+                self.text_box.setText(str(result))
+                self.text_box.setFocus()
+            except ZeroDivisionError:
+                self.text_box.setText('Error: Division by zero')
+                self.text_box.setFocus()
+
+        else:
+            self.text_box.setText(self.text_box.text() + text)
+            self.text_box.setFocus()
+
 
 # Execute app
-main_window.show()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication([])
+    main_window = CalculatorApp()
+    main_window.show()
+    app.exec_()
