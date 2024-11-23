0312516e3d75c29811aea53b1469a4916a70359a
ruyisbaros <ahmet.erdonmez77@gmail.com>
2024-11-23 17:31:21 +0100

    index ui started

--- with_Cmd/main.py
+++ with_Cmd/main.py
@@ -23,7 +23,7 @@ class MainWindow(qtw.QWidget):
         my_entry.setObjectName("name")
         # Add item to layout
         self.layout().addWidget(my_entry, alignment=Qt.AlignCenter) """
-        cmb_box = qtw.QComboBox(self, editable=True,
+        """ cmb_box = qtw.QComboBox(self, editable=True,
                                 insertPolicy=qtw.QCheckBox.insertAction)
         cmb_box.addItem("Doner", "something")
         cmb_box.addItem("Pide", 2)
@@ -35,7 +35,17 @@ class MainWindow(qtw.QWidget):
         cmb_box.addItems(["one", "two"])
         cmb_box.setStyleSheet("QComboBox {width:200px;}")
         # Add item to layout
-        self.layout().addWidget(cmb_box, alignment=Qt.AlignCenter)
+        self.layout().addWidget(cmb_box, alignment=Qt.AlignCenter) """
+        """ spn_box = qtw.QSpinBox(
+            self, value=10, maximum=100, minimum=0, singleStep=5, prefix="#", suffix=" Order")
+        spn_box.setFont(QFont("Helvetica", 15))
+        spn_box.setStyleSheet("QSpinBox {width:200px;}")
+        # Add item to layout
+        self.layout().addWidget(spn_box, alignment=Qt.AlignCenter) """
+        text_box = qtw.QTextEdit(
+            self, lineWrapColumnOrWidth=50, placeholderText="This is a text box")
+
+        self.layout().addWidget(text_box, alignment=Qt.AlignCenter)
         my_button = qtw.QPushButton("Submit", clicked=lambda: press_it())
         my_button.setStyleSheet(
             "QPushButton {font-weight: bold; padding: 10px 20px; background-color: #333; color: #fff; border: none; border-radius:5px} QPushButton:hover {background-color: #666;}"
@@ -48,11 +58,14 @@ class MainWindow(qtw.QWidget):
 
         """ def press_it():
             # cmb_box.currentData(), cmb_box.currentIndex() etc can be selected
-            my_label.setText(f"You picked {cmb_box.currentText()}")
- """
+            my_label.setText(f"You picked {cmb_box.currentText()}")"""
+        """ def press_it():
+            # cmb_box.currentData(), cmb_box.currentIndex() etc can be selected
+            my_label.setText(f"You picked {spn_box.value()}") """
+
         def press_it():
             # cmb_box.currentData(), cmb_box.currentIndex() etc can be selected
-            my_label.setText(f"You picked {cmb_box.currentText()}")
+            my_label.setText(f"You picked {text_box.toPlainText()}")
 
 
 app = qtw.QApplication([])
